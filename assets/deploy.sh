1026987b18360c572b8ee8e1701e6adedc65b9fe
David Schatz <david.schatz@tu-ilmenau.de>
2025-05-18 14:14:01 +0200

    Add update of service file to deploy script

--- assets/deploy.sh
+++ assets/deploy.sh
@@ -11,4 +11,6 @@ src=target/aarch64-unknown-linux-musl/release/menu-ninja
 cargo build --target=aarch64-unknown-linux-musl --release &&
 ssh $host systemctl stop menu-ninja || true
 scp $src $host:/usr/local/bin/menu-ninja
+scp assets/menu-ninja.service $host:/lib/systemd/system/menu-ninja.service
+ssh $host systemctl daemon-reload || true
 ssh $host systemctl restart menu-ninja
